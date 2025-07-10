3174ef8af5576571b181d8650b8ea6c8533f7f33
mrinal1224 <iammrinalkc@gmail.com>
2025-07-10 15:08:58 +0530

    first commit

---
+++ app-enhanced/script.js
@@ -0,0 +1,49 @@
+const form = document.querySelector('form');
+const searchField = document.querySelector('.searchField')
+const tempratureField = document.querySelector('.temp')
+const cityField = document.querySelector('.time_location p')
+const dateField = document.querySelector(".time_location span");
+const emojiField = document.querySelector(".weather-icon img");
+const weatherField = document.querySelector(".weather_condition span");
+
+let target = 'Mumbai'
+
+
+form.addEventListener('submit' , search)
+
+function search(e){
+    e.preventDefault()
+    target = searchField.value
+    console.log(target)
+    fetchData(target)
+}
+
+async function fetchData(target){
+    const endpoint = `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=no`
+
+    const response = await fetch(endpoint)
+
+    const data = await response.json()
+
+    console.log(data)
+
+    let currentTemp = data.current.temp_c
+    let cityName = data.location.name
+    let localTime = data.location.localtime
+
+    let currentCondition = data.current.condition.text
+    let conditonLogo = data.current.condition.icon
+
+    updateWeatherData(currentTemp , cityName , localTime , currentCondition, conditonLogo)
+
+}
+
+function updateWeatherData(currTemp , cityName ,time , currentCondition , conditonLogo){
+    tempratureField.innerText = currTemp
+    cityField.innerText = cityName
+    dateField.innerText = time
+    weatherField.innerText = currentCondition
+    emojiField.src = conditonLogo
+
+
+}
